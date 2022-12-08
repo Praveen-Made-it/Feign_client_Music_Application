329a554dceb9dee299eceb1b3a6d2e869bcbd3bf
Praveen Kumar <praveen170596@gmail.com>
2022-12-08 18:45:19 +0530

    changes done in Customer Application

--- BEJ_C2_S3_REST_API_MONGODB_MC_1/src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/BejC2S3RestApiMongodbMc1Application.java
+++ BEJ_C2_S3_REST_API_MONGODB_MC_1/src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/BejC2S3RestApiMongodbMc1Application.java
@@ -2,8 +2,13 @@ package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
+@EnableEurekaClient
+
 public class BejC2S3RestApiMongodbMc1Application {
 
     public static void main(String[] args) {
