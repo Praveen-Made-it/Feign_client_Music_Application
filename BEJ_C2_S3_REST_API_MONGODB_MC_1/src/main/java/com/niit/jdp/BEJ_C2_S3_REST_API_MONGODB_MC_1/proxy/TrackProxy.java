f89db3070fa8ee6602542590c51ac2f52aeb060c
Praveen Kumar <praveen170596@gmail.com>
2022-12-09 08:43:16 +0530

    added postmapping in TrackProxy class

--- BEJ_C2_S3_REST_API_MONGODB_MC_1/src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/proxy/TrackProxy.java
+++ BEJ_C2_S3_REST_API_MONGODB_MC_1/src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_MC_1/proxy/TrackProxy.java
@@ -1,4 +1,15 @@
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.proxy;
 
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_MC_1.domain.Track;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@FeignClient(name = "authentication-service", url = "authentication-service:8082")
 public interface TrackProxy {
+    @PostMapping("/api/v2/register/")
+    public ResponseEntity<?> saveUser(@RequestBody Track user);
+
 }
